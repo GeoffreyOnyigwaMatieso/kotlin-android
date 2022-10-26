c3a34e902e23ccf2f7648a3f5c684d3bf7488fb6
lynnemunini <muninilynne65@gmail.com>
2022-10-26 17:46:40 +0300

    Working with enums and state

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -820,7 +820,10 @@ class Finder<T> (private val list: List<T>) {
 
 fun main() {
     Repository.startFetch()
+    getResult(result = Repository.getCurrentState())
     Repository.finishedFetch()
+    getResult(result = Repository.getCurrentState())
+    Repository.errorFetch()
     getResult(result = Repository.getCurrentState())
 }
 fun getResult(result: Result) {
@@ -829,7 +832,7 @@ fun getResult(result: Result) {
         Result.FAILURE -> println("Failure")
         Result.ERROR -> println("Error")
         Result.IDLE -> println("Idle")
-        Result.LOADING -> println("Loading")
+        Result.LOADING -> println("Loading...")
     }
 }
 
